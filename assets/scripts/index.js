fa18fe3aa4b762fb5c10f559ab02cb3fd6ac5e89
April <april@dummy.cafe>
2023-07-20 18:08:04 +0200

    add #onboarding fragment

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,3 +5,15 @@ function swapContent(currentContentSelector, newContentSelector) {
 	currentContent.style.display = "none";
 	newContent.style.display = "";
 }
+
+function handleFragment() {
+	console.log(window.location.hash)
+	if (window.location.hash == '#onboarding') {
+		swapContent('div.card.place2023 .main-content', 'div.card.place2023 .template-instructions');
+	} else {
+		swapContent('div.card.place2023 .template-instructions', 'div.card.place2023 .main-content');
+	}
+}
+
+window.onload = handleFragment;
+window.onhashchange = handleFragment;
